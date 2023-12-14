48d35e34a00d0e2a2ce8379bcf7e26a78cafc9ff
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2023-12-14 12:20:07 -0800

    add files and fix them with betty

--- getc_command.c
+++ getc_command.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * gets_command - Extracts a command from user input (char *)
- * @str: User input (char *)
- * @c_command: Pointer to the command to be returned (char **)
- * @cmd_size: Pointer to the size of the command to be returned (int *)
+ * getc_command - extract command from user input
+ * @str: user input (char *)
+ * @c_command: pointer to extracted command (char **)
+ * @cmd_size: pointer to size of extracted command (int *)
  * Return: void
- * by Ahmed Raafat & Ahmed Abdelnasser
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-
-void gets_command(char *str, char **c_command, int *cmd_size)
+void getc_command(char *str, char **c_command, int *cmd_size)
 {
 	int i = 0, j = 0;
 
@@ -47,14 +46,12 @@ void gets_command(char *str, char **c_command, int *cmd_size)
 	alias_change(c_command, cmd_size);
 	edit_comm(c_command, cmd_size);
 }
-
 /**
- * lst_space - Checks if the last char in the string is a space
- * @str: String to check (char *)
- * Return: (1) if the last char in the string is space, (0) otherwise (int)
- * by Ahmed Raafat & Ahmed Abdelnasser
+ * lst_space - verify if the last character in a string is a space
+ * @str: string to examine (char *)
+ * Return: (1) if the last character is a space, (0) otherwise (int)
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-
 int lst_space(char *str)
 {
 	int i = 0;
@@ -66,18 +63,16 @@ int lst_space(char *str)
 	}
 	return (0);
 }
-
 /**
- * gets_comm_help - Helper function for gets_command
- * @str: User input (char *)
- * @c_command: Pointer to the command to be returned (char **)
- * @cmd_size: Pointer to the size of the command to be returned (int *)
- * @i: Pointer to the index of user input (int *)
- * @j: Pointer to the index of the command to be returned (int *)
+ * gets_comm_help - auxiliary function for extracting command
+ * @str: user input (char *)
+ * @c_command: pointer to extracted command (char **)
+ * @cmd_size: pointer to size of extracted command (int *)
+ * @i: pointer to index of user input (int *)
+ * @j: pointer to index of extracted command (int *)
  * Return: void
- * by Ahmed Raafat & Ahmed Abdelnasser
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-
 void gets_comm_help(char *str, char **c_command,
 						 int *cmd_size, int *i, int *j)
 {
@@ -95,15 +90,13 @@ void gets_comm_help(char *str, char **c_command,
 		buffers(NULL, c_command, 1);
 	}
 }
-
 /**
- * edit_comm - Edits the command to replace $$, $?, and $<env>
- * @str_ptr: Pointer to the command to be edited (char **)
- * @str_size: Pointer to the size of the command to be edited (int *)
+ * edit_comm - adjust command to replace $$, $?, and $<env>
+ * @str_ptr: pointer to command to be adjusted (char **)
+ * @str_size: pointer to size of command to be adjusted (int *)
  * Return: void
- * by Ahmed Raafat & Ahmed Abdelnasser
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-
 void edit_comm(char **str_ptr, int *str_size)
 {
 	int i = 0;
@@ -120,7 +113,7 @@ void edit_comm(char **str_ptr, int *str_size)
 			nts(getpid(), pid);
 
 			str = replaceTxtInd(str_ptr, pid, i, i + 1);
-			i += _strllen(pid) - 1;
+			i += _strlen(pid) - 1;
 		}
 		else if (_strcmp(str + i, "$?") == 0)
 		{
@@ -129,7 +122,7 @@ void edit_comm(char **str_ptr, int *str_size)
 			nts(State, last_exit_code);
 
 			str = replaceTxtInd(str_ptr, last_exit_code, i, i + 1);
-			i += _strllen(last_exit_code) - 1;
+			i += _strlen(last_exit_code) - 1;
 		}
 		else if (str[i] == '$' && str[i + 1] != '$' &&
 				 str[i + 1] != ' ' && str[i + 1])
@@ -145,17 +138,15 @@ void edit_comm(char **str_ptr, int *str_size)
 	}
 	_Free(temp);
 }
-
 /**
- * edit_comm_helper - Helper function for edit_comm to replace $<env>
- * @str_ptr: Pointer to the command to be edited (char **) (copy of str)
- * @str: Pointer to the command to be edited (char *) (copy of str_ptr)
- * @temp: Pointer to the temp string (char *) to be used in editing
- * @index: Pointer to the index of the command to be edited (int *) (copy of i)
- * Return: void (edited command is returned through str_ptr)
- * by Ahmed Raafat & Ahmed Abdelnasser
+ * edit_comm_helper - auxiliary function for adjusting command with $<env>
+ * @str_ptr: pointer to command to be adjusted (char **) (copy of str)
+ * @str: pointer to command to be adjusted (char *) (copy of str_ptr)
+ * @temp: pointer to temporary string (char *) for editing
+ * @index: pointer to index of command to be adjusted (int *) (copy of index)
+ * Return: void (adjusted command is returned through command_ptr)
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
  */
-
 void edit_comm_helper(char **str_ptr,
 						 char **str, char *temp, int *index)
 {
@@ -164,13 +155,13 @@ void edit_comm_helper(char **str_ptr,
 
 	while ((*str)[i + j] && (*str)[i + j] != ' ')
 		j++;
-	_memecopy(temp, (*str) + i + 1, j - 1);
+	_memcpy(temp, (*str) + i + 1, j - 1);
 	temp[j - 1] = '\0';
-	value = get_env_value(temp);
+	value = get_envs_val(temp);
 	if (value)
 	{
 		*str = replaceTxtInd(str_ptr, value, i, i + j - 1);
-		*index += _strllen(value) - 1;
+		*index += _strlen(value) - 1;
 		_Free(value);
 	}
 	else
--- gets_input.c
+++ gets_input.c
@@ -1,174 +1,170 @@
 #include "main.h"
 
 /**
-* gets_input - Retrieves input from the user
-* @buff: Pointer to the buffer to store input (char **)
-* @size: Pointer to the size of input to be returned (int *)
-* @buffer_size: Pointer to the size of the buffer to be returned (int *)
-* @source: Source of input (int)
-* Return: void
-* by Ahmed Raafat & Ahmed Abdelnasser
-*/
-void gets_input(char **buff, int *size, int *buffer_size, int source)
+ * get_input - acquire input from user
+ * @buff: pointer to buffer to store input (char **)
+ * @size: pointer to size of input to be returned (int *)
+ * @buffer_size: pointer to size of buffer to be returned (int *)
+ * @source: source of input (int)
+ * Return: void
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
+ */
+void get_input(char **buff, int *size, int *buffer_size, int source)
 {
-int max_read = *buffer_size - 1;
-int read = max_read;
-char *temp = _malloc(max_read + 1);
+	int max_read = *buffer_size - 1;
+	int read = max_read;
+	char *temp = _malloc(max_read + 1);
 
-intail_NULL(temp, max_read + 1);
-while (read == max_read)
-{
-read = _read(source, temp, max_read);
-*size += remove_read_spaces(temp);
+	initialize_NULL(temp, max_read + 1);
+	while (read == max_read)
+	{
+		read = _read(source, temp, max_read);
+		*size += delete_read_spaces(temp);
 
-if (*size > *buffer_size - 1)
-{
-*buff = _realloc(*buff, *size + 1);
-*buffer_size = *size;
+		if (*size > *buffer_size - 1)
+		{
+			*buff = _realloc(*buff, *size + 1);
+			*buffer_size = *size;
+		}
+		_strcat(*buff, temp);
+		buffers(buff, NULL, 1);
+	}
+	_Free(temp);
+	hand_str_spaces(*buff, *buffer_size);
 }
-_strccat(*buff, temp);
-buffers(buff, NULL, 1);
-}
-_Free(temp);
-hand_str_spaces(*buff, *buffer_size);
-}
-
 /**
-* hand_str_spaces - Handles spaces in a string
-* @str: String to handle spaces in (char *)
-* @str_size: Size of the string to handle spaces in (int)
-* Return: void
-* by Ahmed Raafat & Ahmed Abdelnasser
-*/
+ * hand_str_spaces - manage spaces within a string
+ * @str: string to manage spaces in (char *)
+ * @str_size: size of the string to manage spaces in (int)
+ * Return: void
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
+ */
 void hand_str_spaces(char *str, int str_size)
 {
-int i = 0;
-char *temp = _malloc(str_size);
+	int i = 0;
+	char *temp = _malloc(str_size);
 
-intail_NULL(temp, BUFFER_SIZE);
-while (str && str[i])
-{
-if (see_spaces(str, i) == 1)
-{
-_strccpy(temp, str);
-str[i] = ' ';
-_strccpy(str + i + 1, temp + i);
+	initialize_NULL(temp, BUFFER_SIZE);
+	while (str && str[i])
+	{
+		if (see_spaces(str, i) == 1)
+		{
+			_strcpy(temp, str);
+			str[i] = ' ';
+			_strcpy(str + i + 1, temp + i);
+		}
+		else if (see_spaces(str, i) == 2)
+		{
+			_strcpy(temp, str);
+			str[++i] = ' ';
+			_strcpy(str + i + 1, temp + i);
+		}
+		else if (see_spaces(str, i) == 3)
+		{
+			_strcpy(temp, str);
+			i += 2;
+			str[i] = ' ';
+			_strcpy(str + i + 1, temp + i);
+		}
+		i++;
+	}
+	_Free(temp);
 }
-else if (see_spaces(str, i) == 2)
-{
-_strccpy(temp, str);
-str[++i] = ' ';
-_strccpy(str + i + 1, temp + i);
-}
-else if (see_spaces(str, i) == 3)
-{
-_strccpy(temp, str);
-i += 2;
-str[i] = ' ';
-_strccpy(str + i + 1, temp + i);
-}
-i++;
-}
-_Free(temp);
-}
-
 /**
-* remove_read_spaces - Removes spaces from a string
-* @str: String to remove spaces from (char *)
-* Return: Size of the string after removing spaces (int)
-* by Ahmed Raafat & Ahmed Abdelnasser
-*/
-int remove_read_spaces(char *str)
+ * delete_read_spaces - eliminate spaces from a string
+ * @str: string to eliminate spaces from (char *)
+ * Return: size of string after eliminating spaces (int)
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
+ */
+int delete_read_spaces(char *str)
 {
-int strSize = _strllen(str);
-int i = 0;
+	int strSize = _strlen(str);
+	int i = 0;
 
-while (str[i] == ' ')
-i++;
-if (i)
-{
-if (i == strSize - 2)
-{
-intail_NULL(str, strSize);
-return (0);
-}
-else
-{
-char *ptr = str + i;
+	while (str[i] == ' ')
+		i++;
+	if (i)
+	{
+		if (i == strSize - 2)
+		{
+			initialize_NULL(str, strSize);
+			return (0);
+		}
+		else
+		{
+			char *ptr = str + i;
 
-_strccpy(str, ptr);
+			_strcpy(str, ptr);
+		}
+	}
+	remove_read_spaces_hlp(str, strSize);
+	return (_strlen(str));
 }
-}
-remove_read_spaces_hlp(str, strSize);
-return (_strllen(str));
-}
-
 /**
-* remove_read_spaces_hlp - Helper function for remove_read_spaces
-* @str: String to remove spaces from (char *)
-* @strSize: Size of the string to remove spaces from (int)
-* Return: void
-* by Ahmed Raafat & Ahmed Abdelnasser
-*/
+ * remove_read_spaces_hlp - auxiliary function for eliminating spaces
+ * @str: string to eliminate spaces from (char *)
+ * @strSize: size of string to eliminate spaces from (int)
+ * Return: void
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
+ */
 void remove_read_spaces_hlp(char *str, int strSize)
 {
-int i = 0;
+	int i = 0;
 
-while (str[i])
-{
-if (str[i] == ' ' && str[i + 1] == ' ')
-{
-int j = zero_text(str + i + 1);
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] == ' ')
+		{
+			int j = zero_text(str + i + 1);
 
-if (j)
-{
-char *ptr = str + i + 1;
-char *ptr2 = str + i + j + 1;
+			if (j)
+			{
+				char *ptr = str + i + 1;
+				char *ptr2 = str + i + j + 1;
 
-_strccpy(ptr, ptr2);
-}
-else
-{
-char *ptr = str + i;
+				_strcpy(ptr, ptr2);
+			}
+			else
+			{
+				char *ptr = str + i;
 
-strSize = _strllen(str);
-intail_NULL(ptr, strSize);
+				strSize = _strlen(str);
+				initialize_NULL(ptr, strSize);
+			}
+		}
+		i++;
+	}
 }
-}
-i++;
-}
-}
-
 /**
-* see_spaces - Checks if there are spaces in a string
-* @str: String to check (char *)
-* @i: Index to check from (int)
-* Return:
-* (1) if there is a space before ;,
-* (2) if there is a space after ;,
-* (3) if there is a space before && or ||, (0) otherwise (int)
-* by Ahmed Raafat & Ahmed Abdelnasser
-*/
+ * see_spaces - examine spaces within a string
+ * @str: string to inspect (char *)
+ * @i: index to start inspection from (int)
+ * Return:
+ * (1) if there is a space before ;,
+ * (2) if there is a space after ;,
+ * (3) if there is a space before && or ||, (0) otherwise (int)
+* Authors: Ahmed Raafat & Ahmed Abdelanasser
+ */
 int see_spaces(char *str, int i)
 {
-if (i && str[i] == ';' && str[i - 1] != ' ')
-{
-return (1);
-}
-else if (str[i] == ';' && str[i + 1] != ' ')
-{
-return (2);
-}
-else if (_strstr(str + i, "&&") == str + i ||
-_strstr(str + i, "||") == str + i)
-{
-if (i && str[i - 1] != ' ')
-return (1);
-else if (str[i + 2] != ' ')
-return (3);
-else
-return (0);
-}
-else
-return (0);
+	if (i && str[i] == ';' && str[i - 1] != ' ')
+	{
+		return (1);
+	}
+	else if (str[i] == ';' && str[i + 1] != ' ')
+	{
+		return (2);
+	}
+	else if (_strstr(str + i, "&&") == str + i ||
+			 _strstr(str + i, "||") == str + i)
+	{
+		if (i && str[i - 1] != ' ')
+			return (1);
+		else if (str[i + 2] != ' ')
+			return (3);
+		else
+			return (0);
+	}
+	else
+		return (0);
 }
